bedd71d6a3d5bda3f7a6984a0c7bb0bac8eeb25e
kartik120-pk <118261121+kartik120-pk@users.noreply.github.com>
2025-03-18 20:18:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const makeChange = (c) => {
-  // your name here
+    c = parseInt(c); // Convert input to an integer
+
+    return {
+        q: Math.floor(c / 25), // Number of quarters
+        d: Math.floor((c % 25) / 10), // Number of dimes
+        n: Math.floor((c % 25 % 10) / 5), // Number of nickels
+        p: Math.floor(c % 25 % 10 % 5) // Number of pennies
+    };
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
